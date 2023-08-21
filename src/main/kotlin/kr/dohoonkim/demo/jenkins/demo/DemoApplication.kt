b5f4b8ffe990fc70765fbd0e56e72a0c10acb652
dohoon <elensar92@gmail.com>
2023-08-21 18:53:51 +0900

    first commit

---
+++ src/main/kotlin/kr/dohoonkim/demo/jenkins/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package kr.dohoonkim.demo.jenkins.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
---
+++ src/test/kotlin/kr/dohoonkim/demo/jenkins/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.dohoonkim.demo.jenkins.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
