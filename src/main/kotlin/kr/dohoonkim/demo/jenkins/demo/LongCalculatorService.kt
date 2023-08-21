a39ec2490b854b0d60c0cc34abb28cecc65735bb
dohoon <elensar92@gmail.com>
2023-08-22 03:08:16 +0900

    feature : LongCaculatorService 추가

---
+++ src/main/kotlin/kr/dohoonkim/demo/jenkins/demo/LongCalculatorService.kt
@@ -0,0 +1,18 @@
+package kr.dohoonkim.demo.jenkins.demo
+
+import org.springframework.stereotype.Service
+
+@Service
+class LongCalculatorService {
+    fun plus(a : Long, b : Long) = a + b
+
+    fun minus(a : Long, b : Long) = a - b
+
+    fun div(a : Long, b : Long) : Double {
+        if(b == 0L) throw ArithmeticException("Can not divide by zero")
+
+        return a/(b as Double)
+    }
+    fun mul(a : Long, b : Long) = a * b
+
+}
